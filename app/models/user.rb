41574b063cfd31616442913c5eb350fca2e4ffeb
James Park <james.park@annkissam.com>
2014-07-21 20:18:32 -0400

    Created admin (boolean) column in user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :admin, inclusion: { in: [true, false] }
+
+  before_save :non_admin_user
+
+  def non_admin_user
+    self.admin ||= false
+  end
          
 end
