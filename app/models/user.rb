1d82be464de6a87bd0d0f37d15a6a90bf23a9c21
James Park <james.park@annkissam.com>
2014-07-22 10:52:21 -0400

    Removed custom validation in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,9 @@ class User < ActiveRecord::Base
 
   has_many :votes
 
-  before_validation :non_admin_user_default
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  validates :admin, inclusion: { in: [true, false] }
-
-  def non_admin_user_default
-    self.admin = false
-  end
+         
 end
